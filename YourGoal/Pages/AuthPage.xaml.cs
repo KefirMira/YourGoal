9bf834b7012727c2bc6a18e04de62be2e0ac67e0
Sonya <soldatovasonya04@mail.ru>
2023-10-17 16:38:43 +0300

    начало авторизации

---
+++ YourGoal/Pages/AuthPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace YourGoal.Pages
+{
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
