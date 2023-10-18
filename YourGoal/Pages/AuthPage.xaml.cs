a617e065d5a64cc315ae892f0c7b845dc3703173
Sonya <soldatovasonya04@mail.ru>
2023-10-18 12:02:09 +0300

    authorization

--- YourGoal/Pages/AuthPage.xaml.cs
+++ YourGoal/Pages/AuthPage.xaml.cs
@@ -1,4 +1,7 @@
+using System.Windows;
 using System.Windows.Controls;
+using YourGoal.Models;
+using YourGoal.Services;
 
 namespace YourGoal.Pages
 {
@@ -7,6 +10,37 @@ namespace YourGoal.Pages
         public AuthPage()
         {
             InitializeComponent();
+        }
+
+        private void ForgotYourPasswordButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            string Login = LoginTextBox.Text;
+            string Password = PasswordTextBox.Text;
+            AuthService au = new AuthService();
+            User user = au.AuthUser(Login, Password);
+            if (user == null)
+            {
+                MessageBox.Show("Ошибка");
+            }
+            else
+            {
+                MessageBox.Show("!!");
+            }
+        }
+
+        private void EnterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void RegButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
