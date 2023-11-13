ed1fff44f87b222947f269eadfdbfb739958d905
ZHMbIH <anton.dmitriev170105@yandex.ru>
2023-11-13 12:30:30 +0300

    blyat'

---
+++ YourGoal/Pages/GoalsPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace YourGoal.Pages
+{
+    public partial class GoalsPage : Page
+    {
+        public GoalsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
